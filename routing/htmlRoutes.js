0639e3cf19683b0e5bd98d4ff30e5fd9c7418394
lambertco <blambertco@gmail.com>
2019-10-14 19:01:30 -0600

    html routes finished

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,1 +1,15 @@
 var path = require('path');
+
+module.exports = function (app) {
+app.get("/survey", function(req, res) {
+   res.sendFile(__dirname, './Public/survey.html');
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname, './Public/home.html');
+});
+};
+
+
+
+

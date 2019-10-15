76a5586ae8448be13b012316460a0cd7f324ac2c
lambertco <blambertco@gmail.com>
2019-10-14 18:49:59 -0600

    server.js completed

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routing/htmlRoutes')(app);
+require('./routing/apiRoutes')(app);
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
